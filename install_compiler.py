9a1b59606c01e9093f52fa46ea6cd626e4c3d6e7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-16 10:58:57 +0000

    Adding tarfile member sanitization to extractall()

--- install_compiler.py
+++ install_compiler.py
@@ -315,7 +315,26 @@ def install_binutils(base_path, install_path, num_threads):
             sys.exit(1)
         else:
             with tarfile.open('binutils-2.27.tar.bz2', 'r:bz2') as f:
-                f.extractall(path=os.path.join(install_path, 'src'))
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, path=os.path.join(install_path,"src"))
 
 
         # PATCH BINUTILS
